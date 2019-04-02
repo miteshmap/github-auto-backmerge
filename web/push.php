770a262440479e536d52ed2c88ae2705dd947113
vbouchet <vincent.bouchet.31@gmail.com>
2019-04-02 17:08:32 +0200

    Update code

--- web/push.php
+++ web/push.php
@@ -3,5 +3,5 @@
 use Cz\Git\GitRepository;
 
 function webhook_push_callback($payload) {
-  $repo = GitRepository::cloneRepository('https://alshaya-github-bot:Alshaya%402019&github.com/acquia-pso/alshaya', '/tmp/alshaya');
+  $repo = GitRepository::cloneRepository('https://github.com/acquia-pso/alshaya', '/tmp/alshaya');
 }
