ef9f44aa4b59386af33c8c5b10f3d0a4523bce65
vbouchet <vincent.bouchet.31@gmail.com>
2019-04-03 14:58:46 +0200

    Update code

--- web/push.php
+++ web/push.php
@@ -2,21 +2,38 @@
 
 use Cz\Git\GitRepository;
 
+$upstream_branches = [
+  'master' => 'uat',
+  'uat' => 'qa',
+  'qa' => 'develop',
+];
+
 function webhook_push_callback($payload) {
-  error_log(var_export($payload, 1));
-  error_log('ici');
+  // Get the source branch from the payload.
+  $branch = str_replace('refs/heads/', '', $payload->ref);
 
+  $repo = init_git_repository();
+  $branches = $repo->getBranches();
+
+  error_log(var_export($branches));
+
+  foreach ($branches as $branch) {
+    
+  }
+}
+
+function init_git_repository() {
   $dir = '/tmp/alshaya';
-
   delete_directory($dir);
 
+  $repo = FALSE;
   try {
     $repo = GitRepository::cloneRepository('git+ssh://git@github.com/vbouchet31/test-php-git.git', $dir);
+    $repo->fetch();
   }
-  catch (Exception $e) {
-    var_dump($e);
-  }
+  catch (Exception $e) {}
 
+  return $repo;
 }
 
 function delete_directory($dir) {
