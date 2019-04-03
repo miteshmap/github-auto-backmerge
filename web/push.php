5041687f405b877d6c995d046c926151fdcba686
vbouchet <vincent.bouchet.31@gmail.com>
2019-04-03 10:44:03 +0200

    Update code

--- web/push.php
+++ web/push.php
@@ -5,14 +5,14 @@ use Cz\Git\GitRepository;
 function webhook_push_callback($payload) {
   $dir = '/tmp/alshaya';
 
-  rmdir($dir);
+  if (is_dir($dir)) {
+    rmdir($dir);
+  }
 
   try {
-    $repo = GitRepository::cloneRepository('git+ssh://git@github.com/acquia-pso/alshaya.git', $dir);
-    var_dump($repo->getBranches());
+    $repo = GitRepository::cloneRepository('git+ssh://git@github.com:vbouchet31/test-php-git.git', $dir);
+    var_dump($payload);
     $repo->fetch();
-    //$repo->createBranch('vbo-test');
-    //$repo->push();
   }
   catch (Exception $e) {
     var_dump($e);
